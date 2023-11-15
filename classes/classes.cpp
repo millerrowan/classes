042b9cbd30a22cbae9ed5f97a8fad2f1e05a5715
millerrowan <144379880+millerrowan@users.noreply.github.com>
2023-11-14 23:25:19 -0800

    Add files via upload

--- classes/classes.cpp
+++ classes/classes.cpp
@@ -6,12 +6,17 @@
 
 using namespace std;
 
+
+void add(vector<media*>* mediatypes, char input[10]);
+void search(vector<media*>* mediatypes);
+void erase(vector<media*>* mediatypes); 
+
 int main() {
   //includes where you create new Parent and well as new childs
   //in the add function ask which media type person would like to create and create a pointer of that media type
  bool stillUsing = true;
 
-  vector <media*> mediatypes;
+  vector <media*>* mediatypes;
 
   while (stillUsing == true)  {
     cout << "Please type in either ADD, SEARCH, DELETE, or QUIT" << endl;
@@ -44,7 +49,7 @@ int main() {
   return 0; 
 }
 
-void add(vector<media*>& mediatypes, char input[10]) {
+void add(vector<media*>* mediatypes, char input[10]) {
   cout << "what media type would you like to add?" << endl;
   cout << "type 'videogame', 'music', or 'movie'" << endl;
   cin >> input;
@@ -61,16 +66,70 @@ void add(vector<media*>& mediatypes, char input[10]) {
 
 }
 
-//prints student
-void search(vector<media*>& mediatypes) {
+//search for a print media
+void search(vector<media*>* mediatypes) {
   cout << "search for media" << endl;
+  cout << "Would you like to search by year or by title" << endl;
+  cout << "Type YEAR or TITLE" << endl;
+  char input[10];
+  cin >> input;
+  cin.ignore();
+  if (strcmp(input, "YEAR") == 0) {
+    int inputYear;
+    cout << "enter a year" << endl;
+    cin >> inputYear;
+    for(vector<media*>::iterator it = mediatypes.begin(); it != mediatypes.end(); it++) {
+      if((*it) -> year == inputYear) {
+	//create print function
+      }
+    }
+  }
+  else if (strcmp(input, "TITLE") == 0) {
+    char inputTitle[30];
+    cout << "Enter a title" << endl;
+    cin >> inputTitle;
+    cin.ignore(); 
+    for(vector<media*>::iterator it = mediatypes.begin(); it != mediatypes.end(); it++) {
+      if((*it) -> year == inputTitle) {
+        //create print function
+      }
+    }
+
+}
+
+ 
+//deletes media from the database
+void erase(vector<media*>& mediatypes) {
+  cout << "deleting a media type" << endl;
+  cout << "Would you like to search by year or by title" << endl;
+  cout << "Type YEAR or TITLE" << endl;
+  char input[10];
+  cin >> input;
+  cin.ignore();
+  if (strcmp(input, "YEAR") == 0) {
+    int inputYear;
+    cout << "enter a year" << endl;
+    cin >> inputYear;
+    for(vector<media*>::iterator it = mediatypes.begin(); it != mediatypes.end(); it++) {
+      if((*it) -> year == inputYear) {
+        delete *it;
+	mediatypes.erase(it);
+	break;
+      }
+    }
+  }
+  else if (strcmp(input, "TITLE") == 0) {
+    char inputTitle[30];
+    cout << "Enter a title" << endl;
+    cin >> inputTitle;
+    cin.ignore();
+    for(vector<media*>::iterator it = mediatypes.begin(); it != mediatypes.end(); it++) {
+      if((*it) -> year == inputTitle) {
+        delete *it;
+	mediatypes.erase(it);
+	break;
+      }
+    }
   }
 }
 
-//deletes students from the database
-void erase(vector<media*>& mediatypes) {
-  
-      break;
-    }
-   }
-}
